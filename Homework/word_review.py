8872f75a65f3f411cfcfed0ea64f21c18b18bc7b
Taylor webb <168122992+Taylorscottwebb@users.noreply.github.com>
2024-05-20 20:36:58 -0500

    Update word_review.py

--- Homework/word_review.py
+++ Homework/word_review.py
@@ -42,3 +42,6 @@ def pos_and_neg_count():
                 number_of_negative += 1
     print(f" The number of negatives are {number_of_negative}.")
 pos_and_neg_count()
+
+review = "This product is really good... I'm impressed with its quality., The performance of this product is excellent. Highly recommended!,I had a bad experience with this product. It didn't meet my expectations., Poor quality product. Wouldn't recommend it to anyone., The product was average. Nothing extraordinary about it." 
+print(review[:31])
